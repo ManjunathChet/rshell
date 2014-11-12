79c2ebd1efb5854df8c576f5a7c27bb314bd9846
Chetas Manjunath <cmanj001@ucr.edu>
2014-11-11 20:22:09 -0800

    Fixed stat syscall error in main

--- src/ls.cpp
+++ src/ls.cpp
@@ -58,6 +58,80 @@ int ls( const char * dirName, bool a_flag, bool l_flag, bool R_flag)
     }
     //char const *dirName = ".";
 
+    struct stat tester;
+    if (stat(dirName, &tester) == -1)
+    {
+        perror("stat");
+    }
+
+    if (S_ISREG(tester.st_mode) == true && l_flag == false)
+    {
+        cout<<dirName;
+        cout<<endl<<endl;
+        return 0;
+    }
+    else if (S_ISREG(tester.st_mode) == true && l_flag == true)
+    {
+        struct stat sb;
+        if(stat(dirName, &sb)==-1)
+        {
+            perror("stat");
+        }
+
+        switch (sb.st_mode & S_IFMT) {
+        case S_IFBLK:  cout<<"b";       break;
+        case S_IFCHR:  cout<<"c";       break;
+        case S_IFDIR:  cout<<"d";       break;
+        case S_IFIFO:  cout<<"p";       break;
+        case S_IFLNK:  cout<<"l";       break;
+        case S_IFREG:  cout<<"-";       break;
+        case S_IFSOCK: cout<<"s";       break;
+        default:       cout<<"?";       break;
+        }
+       
+        if((sb.st_mode & S_IRUSR))
+        {
+            cout<<"r";
+        }
+
+        if((sb.st_mode & S_IWUSR))
+        {
+            cout<<"w";
+        }
+
+        if((sb.st_mode & S_IXUSR))
+        {
+            cout<<"x";
+        }               
+        
+        struct passwd *user = getpwuid(getuid());
+        if (user == NULL)
+        {
+            perror("get user error");
+        }
+        
+        struct group *grp = getgrgid(getgid());
+        if (grp == NULL)
+        {
+            perror("getgid error");
+        }
+
+        string time = ctime(&sb.st_mtime);
+        string newtime = time.substr(0,24);
+
+            cout<<right<<setw(6)<<setfill('-');
+            cout<<sb.st_nlink;
+            cout<<setw(8)<<right<<setfill(' ')
+                <<sb.st_size<<" ";
+            cout<<setw(8)<<right<<user -> pw_name<<" "
+                <<grp -> gr_name<<" ";
+            cout<<setw(10)<<right<<newtime<<" ";
+            cout<<dirName<<endl;
+            return 0;
+    }
+
+
+
     DIR *dirp = opendir(dirName);       
     //dirp is a pointer to directory stream
     if(dirp == NULL)                    
@@ -69,7 +143,8 @@ int ls( const char * dirName, bool a_flag, bool l_flag, bool R_flag)
     dirent *direntp;
     int longest= 0;
     vector <string> filenames;
-
+    
+    
     while ((direntp = readdir(dirp)))
     {
     
@@ -102,44 +177,6 @@ int ls( const char * dirName, bool a_flag, bool l_flag, bool R_flag)
                 filenames.pop_back();
             }
         }
-
-//
-//            cout<<current<<endl;
-//            printf("File type:                ");
-//
-//            switch (sb.st_mode & S_IFMT) {
-//                case S_IFBLK:  printf("block device\n");            break;
-//                case S_IFCHR:  printf("character device\n");        break;
-//                case S_IFDIR:  printf("directory\n");               break;
-//                case S_IFIFO:  printf("FIFO/pipe\n");               break;
-//                case S_IFLNK:  printf("symlink\n");                 break;
-//                case S_IFREG:  printf("regular file\n");            break;
-//                case S_IFSOCK: printf("socket\n");                  break;
-//                default:       printf("unknown?\n");                break;
-//            }
-//
-//            printf("I-node number:            %ld\n", (long) sb.st_ino);
-//
-//            printf("Mode:                     %lo (octal)\n",
-//                    (unsigned long) sb.st_mode);
-//
-//            printf("Link count:               %ld\n", (long) sb.st_nlink);
-//            printf("Ownership:                UID=%ld   GID=%ld\n",
-//                    (long) sb.st_uid, (long) sb.st_gid);
-//
-//            printf("Preferred I/O block size: %ld bytes\n",
-//                    (long) sb.st_blksize);
-//            printf("File size:                %ld bytes\n",
-//                    (long) sb.st_size);
-//            printf("Blocks allocated:         %ld\n",
-//                    (long) sb.st_blocks);
-//
-//            printf("Last status change:       %s", ctime(&sb.st_ctime));
-//            printf("Last file access:         %s", ctime(&sb.st_atime));
-//            printf("Last file modification:   %s", ctime(&sb.st_mtime));       
-//            cout<<endl<<endl;
-
-        
     }
     
     if (R_flag == true)
@@ -251,14 +288,13 @@ int ls( const char * dirName, bool a_flag, bool l_flag, bool R_flag)
                     perror("stat");
                 }
 
-                 switch (sb.st_mode & S_IFMT) 
-                 {
-                    case S_IFDIR:
-                        ls(filenames[i].c_str(), 
+                if ((sb.st_mode & S_IFMT) == S_IFDIR) 
+                {
+                    ls(filenames[i].c_str(), 
                                 a_flag, 
                                 l_flag, 
-                                R_flag); break;
-                 }
+                                R_flag);
+                }
             }
         }
 
@@ -270,76 +306,70 @@ int ls( const char * dirName, bool a_flag, bool l_flag, bool R_flag)
     return 0;
 }
 
-    int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+{
+    if (argc < 1)
     {
-        if (argc < 1)
+        cerr<<"No argument."<<endl;
+    }
+
+    vector <char *> dirlist;
+
+    bool a_flag = false;
+    bool R_flag = false;
+    bool l_flag = false;
+
+    for (int i = 1; i < argc; i++)
+    {
+        char* command = argv[i];
+        struct stat sb; 
+
+        if( stat (argv[i], &sb) != 0 && command[0] != '-')
         {
-            cerr<<"No argument."<<endl;
+            cerr<<"Input does not exist. Filename entered: "<<argv[i] <<endl;
+            return -1;
         }
 
-        vector <char *> dirlist;
-
-        bool a_flag = false;
-        bool R_flag = false;
-        bool l_flag = false;
-
-        for (int i = 1; i < argc; i++)
+        else if (command[0] == '-')
         {
-            char* command = argv[i];
-            struct stat sb; 
-
-            if( stat (argv[i], &sb) != 0 && command[0] != '-')
+            for (int j = 1; command[j] != '\0'; j++)
             {
-                cerr<<"Input does not exist. Filename entered: "<<argv[i] <<endl;
-                return -1;
-            }
-
-            else if (command[0] == '-')
-            {
-                for (int j = 1; command[j] != '\0'; j++)
+                if (command[j] == 'a')
                 {
-                    if (command[j] == 'a')
-                    {
-                        a_flag = true;
-                    }
-                    else if (command[j] == 'l')
-                    {
-                        l_flag = true;
-                    }
-                    else if (command[j] == 'R')
-                    {
-                        R_flag = true;
-                    }
-                    else
-                    {
-                        cerr<<"Invalid option --- '"
-                            <<command[j]<<"' "<<endl;
+                    a_flag = true;
+                }
+                else if (command[j] == 'l')
+                {
+                    l_flag = true;
+                }
+                else if (command[j] == 'R')
+                {
+                    R_flag = true;
+                }
+                else
+                {
+                    cerr<<"Invalid option --- '"
+                        <<command[j]<<"' "<<endl;
 
-                        return -1;
-                    }
+                    return -1;
                 }
             }
-
-            if( S_ISDIR(sb.st_mode))
-            {
-                dirlist.push_back(argv[i]);
-            }
-            else if (S_ISREG(sb.st_mode))
-            {
-                cout<<argv[i];
-                cout<<endl;
-                return 0;
-            }
         }
-        for(unsigned int i = 0; i < dirlist.size(); i++)
+        else
         {
-            ls(dirlist.at(i),a_flag, l_flag, R_flag);
+            dirlist.push_back(argv[i]);
         }
-
-        if (dirlist.empty())
-        {
-            ls(".", a_flag, l_flag, R_flag);
-        }
-
-        return 0;
     }
+    
+    for(unsigned int i = 0; i < dirlist.size(); i++)
+    {
+        ls(dirlist.at(i),a_flag, l_flag, R_flag);
+    }
+
+    if (dirlist.empty())
+    {
+        ls(".", a_flag, l_flag, R_flag);
+    }
+
+    return 0;
+}
